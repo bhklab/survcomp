d3aac56a9a24ed915e107d75d79c0ba3ea96151c
colsen <colsen@ulb.ac.be>
2011-11-15 10:22:50 +0100

    small change in wrapper function for mrmr ensemble

--- R/concordance.index.mrmr.ensemble.R
+++ R/concordance.index.mrmr.ensemble.R
@@ -5,7 +5,7 @@ function(mydata, surv.time, surv.event, cl, weights, comppairs=10, strat, alpha=
 	nsample<-nrow(mydata)
 	
 	
-	vec_ensemble<-.Call("mrmr_cIndex_ensemble_remove",data.matrix(mydata),as.integer(is.na(mydata)),maxparents,ncol(mydata),nrow(mydata),1,1,nboot,maxnsol,-1000,as.integer(as.logical(TRUE)),as.integer(sort(unique(strat))),as.integer(cl ),as.double(surv.time),as.integer(surv.event),as.double(weights),as.integer(strat),as.integer(sum(weight)),as.integer(as.logical(TRUE)),as.integer(length(strat)),as.integer(length(sort(unique(strat)))))
+	vec_ensemble<-.Call("mrmr_cIndex_ensemble_remove",data.matrix(mydata),as.integer(is.na(mydata)),maxparents,ncol(mydata),nrow(mydata),1,1,nboot,maxnsol,-1000,as.integer(as.logical(TRUE)),as.integer(sort(unique(strat))),as.integer(cl ),as.double(surv.time),as.integer(surv.event),as.double(weights),as.integer(strat),as.integer(sum(weight)),as.integer(as.logical(outx)),as.integer(length(strat)),as.integer(length(sort(unique(strat)))))
 	
 	vec_ensemble[2:vec_ensemble[1]+1]<-vec_ensemble[2:vec_ensemble[1]+1]-1
 	models.equiv <- .extract.all.parents(mydata,vec_ensemble,maxparents,1)
