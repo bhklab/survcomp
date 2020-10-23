bdd92bd47febed14573ada3187497a9c9cab202b
ChristopherEeles <christopher.eeles@outlook.com>
2020-10-23 15:00:04 -0400

    Update: roxygen2 for balanced.hazard.ratio

--- R/balanced.hazard.ratio.R
+++ R/balanced.hazard.ratio.R
@@ -1,3 +1,64 @@
+#' @name balanced.harzard.ratio
+#'
+#' @title Function to estimate the balanced hazard ratio through Cox regression
+#'
+#' @description
+#' Function to compute the balanced hazard ratio for a risk group prediction.
+#' The balanced hazard ratio is computed using the Cox model.
+#'
+#' @usage
+#' balanced.hazard.ratio(x, surv.time, surv.event, alpha = 0.05,
+#' method.test = c("logrank", "likelihood.ratio", "wald"),
+#' ties = c("efron", "breslow", "exact"), weights, strat, ...)
+#'
+#' @param x a vector of risk group predictions.
+#' @param surv.time a vector of event times.
+#' @param surv.event a vector of event occurrence indicators.
+#' @param alpha alpha level to compute confidence interval.
+#' @param method.test ...
+#' @param ties ...
+#' @param weights ...
+#' @param strat ...
+#' @param ... Additional parmaeters to the hazard.ratio and coxph functions.
+#'
+#' @return
+#' A list with the items:
+#' - balanced.hazard.ratio: balanced hazard ratio estimate.
+#' - coef: coefficient (beta) estimated in the cox regression model.
+#' - se: standard error of the coefficient (beta) estimate.
+#' - lower: lower bound for the confidence interval.
+#' - upper: upper bound for the confidence interval.
+#' - p.value: p-value computed using the score (logrank) test whether the
+#' balanced hazard ratio is different from 1.
+#' - n: number of samples used for the estimation.
+#' - coxm: coxph.object fitted on the survival data and x.
+#' - data: list of data used to compute the balanced hazard ratio (x,
+#' surv.time and surv.event).
+#'
+#' @authors
+#' Samuel Branders
+#'
+#' @references
+#' Branders, S. and Dupont, P. (2015) "A balanced hazard ratio for risk group
+#'   evaluation from survival data", Statistics in Medicine, 34(17),
+#'   pages 2528–2543.
+#'
+#' @seealso
+#' [survcomp::hazard.ratio], [survival::coxph], [survival::coxph.object]
+#'
+#' @examples
+#' set.seed(12345)
+#' age <- rnorm(100, 50, 10)
+#' stime <- rexp(100)
+#' cens   <- runif(100,.5,2)
+#' sevent  <- as.numeric(stime <= cens)
+#' stime <- pmin(stime, cens)
+#' strat <- sample(1:3, 100, replace=TRUE)
+#' weight <- runif(100, min=0, max=1)
+#' balanced.hazard.ratio(x=age, surv.time=stime, surv.event=sevent,
+#'   weights=weight, strat=strat)
+#'
+#' @export
 balanced.hazard.ratio <-
 function(x, surv.time, surv.event, alpha=0.05, method.test = c("logrank", "likelihood.ratio", "wald"), ties=c("efron","breslow","exact"), weights, strat, ...)
 {
