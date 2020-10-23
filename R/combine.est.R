3f5367982ca8c4b717aedd71469bdcb694ab38d9
ChristopherEeles <christopher.eeles@outlook.com>
2020-10-23 15:49:30 -0400

    Update: roxygen2 for combine.est

--- R/combine.est.R
+++ R/combine.est.R
@@ -1,3 +1,51 @@
+#' @name combine.est
+#'
+#' @title Function to combine estimates
+#'
+#' @description
+#' The function combines several estimators using meta-analytical formula to
+#'   compute a meta-estimate.
+#'
+#' @usage combine.est(x, x.se, hetero = FALSE, na.rm = FALSE)
+#'
+#' @param x vector of estimates
+#' @param x.se vector of standard errors of the corresponding estimates
+#' @param hetero `TRUE` is the heterogeneity should be taken into account
+#'   (random effect model), `FALSE` otherwise (fixed effect model)
+#' @param na.rm TRUE if the missing values should be removed from the data,
+#'   `FALSE` otherwise.
+#'
+#' @return
+#' A list with items:
+#' - estimate: meta-estimate
+#' - se: standard error of the meta-estimate
+#'
+#' @authors
+#' Benjamin Haibe-Kains
+#'
+#' @references
+#' Cochrane, W. G. (1954) "The combination of estimates from different
+#'   experiments", Biometrics, 10, pages 101–129.
+#'
+#' @seealso
+#' [survcomp::test.hetero.est]
+#'
+#' @examples
+#' set.seed(12345)
+#' x1 <- rnorm(100, 50, 10) + rnorm(100, 0, 2)
+#' m1 <- mean(x1)
+#' se1 <- sqrt(var(x1))
+#' x2 <- rnorm(100, 75, 15) + rnorm(100, 0, 5)
+#' m2 <- mean(x2)
+#' se2 <- sqrt(var(x2))
+#'
+#' #fixed effect model
+#' combine.est(x=c(m1, m2), x.se=c(se1, se2), hetero=FALSE)
+#' #random effect model
+#' combine.est(x=c(m1, m2), x.se=c(se1, se2), hetero=TRUE)
+#'
+#' @md
+#' @export
 combine.est <-
 function(x, x.se, hetero=FALSE, na.rm=FALSE) {
 	cc.ix <- complete.cases(x, x.se)
