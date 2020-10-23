08965a4e90814532c211f2ebf8948452580134d3
ChristopherEeles <christopher.eeles@outlook.com>
2020-10-23 15:11:35 -0400

    Update: roxygen2 docs for censor.time

--- R/censor.time.R
+++ R/censor.time.R
@@ -1,3 +1,35 @@
+#' @name censor.time
+#'
+#' @title Function to artificially censor survival data
+#'
+#' @description
+#' The function censors the survival data at a specific point in time. This is
+#'   is useful if you used datasets having different follow-up periods.
+#'
+#' @usage
+#' censor.time(surv.time, surv.event, time.cens = 0)
+#'
+#' @param surv.time vector of times to event occurrence
+#' @param surv.event vector of indicators for event occurrence
+#' @param time.cens point in time at which the survival data must be censored
+#'
+#' @return
+#' A list with the items:
+#' - surv.time.cens: vector of censored times to event occurrence
+#' - srv.event.cens: vector of censored indicators for event occurrence
+#'
+#' @authors
+#' Benjamin Haibe-Kains
+#'
+#' @examples
+#' set.seed(12345)
+#' stime <- rexp(30)
+#' cens <- runif(30,0.5,2)
+#' sevent <- as.numeric(stime <= cens)
+#' stime <- pmin(stime, cens)
+#' censor.time(surv.time=stime, surv.event=sevent, time.cens=1)
+#'
+#' @export
 censor.time <-
 function(surv.time, surv.event, time.cens=0) {
 	stc <- surv.time
