21d81b66baf0ed2285978ff582e8a31c78c5e580
ChristopherEeles <christopher.eeles@outlook.com>
2020-10-23 16:56:18 -0400

    Update: roxygen2 for hazard.ratio

--- R/hazard.ratio.R
+++ R/hazard.ratio.R
@@ -1,3 +1,65 @@
+#' @name hazard.ratio
+#'
+#' @title Function to estimate the hazard ratio through Cox regression
+#'
+#' @description
+#' Function to compute the hazard ratio for a risk prediction.
+#'
+#' @usage
+#' hazard.ratio(x, surv.time, surv.event, weights, strat, alpha = 0.05,
+#' method.test = c("logrank", "likelihood.ratio", "wald"), na.rm = FALSE, ...)
+#'
+#' @param x a vector of risk predictions.
+#' @param surv.time a vector of event times.
+#' @param surv.event a vector of event occurrence indicators.
+#' @param weights weight of each sample.
+#' @param strat stratification indicator.
+#' @param alpha alpha level to compute confidence interval.
+#' @param method.test Statistical test to use in order to compute the p-values
+#'   related to a D.index, see [survival::summary.coxph] for more details.
+#' @param na.rm `TRUE` if missing values should be removed.
+#' @param ... additional parameters to be passed to the [survival::coxph]
+#'   function.
+#'
+#' @details
+#' The hazard ratio is computed using the Cox model.
+#'
+#' @return
+#' A list with items:
+#' - hazard.ratio: hazard ratio estimate.
+#' - coef: coefficient (beta) estimated in the cox regression model.
+#' - se: standard error of the coefficient (beta) estimate.
+#' - lower: upper bound for the confidence interval.
+#' - upper: upper bound for the confidence interval.
+#' - p.value: p-value computed using the likelihood ratio test whether the hazard ratio is different from 1.
+#' - n: number of samples used for the estimation.
+#' - data: list of data used to compute the hazard ratio (x, surv.time and
+#' surv.event).
+#'
+#' @authors
+#' Benjamin Haib-Kains
+#'
+#' @reference
+#' Cox, D. R. (1972) "Regression Models and Life Tables", Journal of the Royal
+#'   Statistical Society Series B, 34, pages 187–220.
+#'
+#' @seealso
+#' [survival::coxph], [survival::coxph.object]
+#'
+#' @examples
+#' set.seed(12345)
+#' age <- rnorm(100, 50, 10)
+#' stime <- rexp(100)
+#' cens   <- runif(100,.5,2)
+#' sevent  <- as.numeric(stime <= cens)
+#' stime <- pmin(stime, cens)
+#' strat <- sample(1:3, 100, replace=TRUE)
+#' weight <- runif(100, min=0, max=1)
+#' hazard.ratio(x=age, surv.time=stime, surv.event=sevent, weights=weight,
+#'   strat=strat)
+#'
+#' @md
+#' @export
 hazard.ratio <-
 function(x, surv.time, surv.event, weights, strat, alpha=0.05, method.test=c("logrank", "likelihood.ratio", "wald"), na.rm=FALSE, ...) {
 	method.test <- match.arg(method.test)
